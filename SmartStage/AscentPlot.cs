4354aa41cfb6f6c47e451c267aae56b1ec20b53c
Xytovl <xytovl-git@laposte.net>
2014-12-09 19:57:46 +0100

    Improve plot
    Use Bresenham's line drawing algorithm

--- SmartStage/AscentPlot.cs
+++ SmartStage/AscentPlot.cs
@@ -48,7 +48,10 @@ namespace SmartStage
 				double valToY = (texture.height - 1) / values.Max();
 				for (int i = 1 ; i < time.Count() ; i++)
 				{
-					texture.SetPixel((int)(time[i] * timeToX), (int)(values[i] * valToY), pulsed);
+					drawLine(texture,
+						(int)(time[i-1] * timeToX), (int)(values[i-1] * valToY),
+						(int)(time[i] * timeToX), (int)(values[i] * valToY),
+						pulsed);
 				}
 			}
 		}
@@ -62,6 +65,48 @@ namespace SmartStage
 			plots.Add(new PlotElement("velocity", times, samples.ConvertAll(s => s.velocity), new Color(1, 0.3f, 0.3f)));
 			texture = new Texture2D(xdim, ydim, TextureFormat.RGB24, false);
 			drawTexture();
+		}
+
+		private static void swap(ref int a, ref int b)
+		{
+			int tmp = a;
+			a = b;
+			b = tmp;
+		}
+
+		private static void drawLine(Texture2D texture, int x0, int y0, int x1, int y1, Color colour)
+		{
+			bool steep = Math.Abs(y1 - y0) > Math.Abs(x1 - x0);
+			if (steep)
+			{
+				swap(ref x0, ref y0);
+				swap(ref x1, ref y1);
+			}
+			if (x0 > x1)
+			{
+				swap(ref x0, ref x1);
+				swap(ref y0, ref y1);
+			}
+
+			int dX = (x1 - x0);
+			int dY = Math.Abs(y1 - y0);
+			int err = (dX / 2);
+			int ystep = (y0 < y1 ? 1 : -1);
+			int y = y0;
+
+			for (int x = x0; x <= x1; ++x)
+			{
+				if (steep)
+					texture.SetPixel(y, x, colour);
+				else
+					texture.SetPixel(x, y, colour);
+				err = err - dY;
+				if (err < 0)
+				{
+					y += ystep;
+					err += dX;
+				}
+			}
 		}
 
 		private void drawTexture()
